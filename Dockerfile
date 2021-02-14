9c765d2f6299d67387701e034ae4f2144d86775c
Jared Hampson <jhampson.dbre@gmail.com>
2021-02-14 14:26:54 -0600

    Update requirements and settings for using mariaDB backend

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM python:3
+FROM python:3.9-buster
 RUN mkdir -p /usr/src/blogproject
 WORKDIR /usr/src/blogproject
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONBUFFERED 1
+# install mariadb dependencies
+RUN apt-get update \
+    && apt-get -y upgrade \
+    && apt-get install -y default-libmysqlclient-dev build-essential
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY blogproject/ .
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / 'db.sqlite3'),
+        "USER": os.environ.get("SQL_USER", ""),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", ""),
+        "HOST": os.environ.get("SQL_HOST", ""),
+        "PORT": os.environ.get("SQL_PORT", ""),
     }
 }
 
